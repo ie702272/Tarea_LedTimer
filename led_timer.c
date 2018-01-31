639b9133ffa140abf0c613406b300477defee64d
ie702272 <ie702272@iteso.mx>
2018-01-31 14:29:14 -0600

    Inicializaciones
    LEDs y switch

--- led_timer.c
+++ led_timer.c
@@ -45,22 +45,82 @@
 /*
  * @brief   Application entry point.
  */
-int main(void) {
+ void PORTA_IRQHandler()
+    {
+    	static uint8_t state = 0;
+    	PORT_ClearPinsInterruptFlags(PORTA, 1<<4);
 
-  	/* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
-  	/* Init FSL debug console. */
-    BOARD_InitDebugConsole();
-
-    printf("Hello World\n");
-
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
+    	GPIO_WritePinOutput(GPIOB,21,state);
+    	state = ( 0 == state ) ? 1 : 0;
     }
-    return 0 ;
+
+    int main(void)
+    {
+
+    	/* Init board hardware. */
+    	BOARD_InitBootPins();
+    	BOARD_InitBootClocks();
+    	BOARD_InitBootPeripherals();
+    	/* Init FSL debug console. */
+    	BOARD_InitDebugConsole();
+
+    	CLOCK_EnableClock(kCLOCK_PortB);
+    	CLOCK_EnableClock(kCLOCK_PortA);
+
+    	port_pin_config_t config_led =
+    	{ kPORT_PullDisable,
+    	  kPORT_SlowSlewRate,
+		  kPORT_PassiveFilterDisable,
+		  kPORT_OpenDrainDisable,
+		  kPORT_LowDriveStrength,
+		  kPORT_MuxAsGpio,
+    	  kPORT_UnlockRegister,
+    	};
+
+    	PORT_SetPinConfig(PORTB, 21, &config_led);
+
+    	port_pin_config_t config_switch =
+    	{ kPORT_PullDisable,
+    	  kPORT_SlowSlewRate,
+		  kPORT_PassiveFilterDisable,
+    	  kPORT_OpenDrainDisable,
+		  kPORT_LowDriveStrength,
+		  kPORT_MuxAsGpio,
+    	  kPORT_UnlockRegister
+    	};
+
+    	PORT_SetPinInterruptConfig(PORTA, 4, kPORT_InterruptFallingEdge);
+
+    	PORT_SetPinConfig(PORTA, 4, &config_switch);
+
+    	gpio_pin_config_t led_config_gpio =
+    	{
+    			kGPIO_DigitalOutput, 1
+    	};
+
+    	GPIO_PinInit(GPIOB, 21, &led_config_gpio);
+
+    	gpio_pin_config_t switch_config_gpio =
+    	{
+    			kGPIO_DigitalInput, 1
+    	};
+
+    	GPIO_PinInit(GPIOA, 4, &switch_config_gpio);
+
+    	NVIC_EnableIRQ(PORTA_IRQn);
+
+    	/* Force the counter to be placed into memory. */
+    	volatile static int i = 0;
+    	/* Enter an infinite loop, just incrementing a counter. */
+
+    	PRINTF("Hola mundo!");
+
+    	GPIO_WritePinOutput(GPIOB,21,0);
+
+        while(1)
+        {
+            i++ ;
+        }
+
+        return 0 ;
 }
